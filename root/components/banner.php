d7e214678f46c169db984a835c0d554c2cfe081f
leoyoon17 <l.yoon17@gmail.com>
2021-01-04 15:33:31 -0700

    Added banner to index and created banner component

---
+++ root/components/banner.php
@@ -0,0 +1,9 @@
+<!-- Banner -->
+<div class="my-banner-container">
+    <div class="banner-profile-picture">
+    </div>
+    <div class="my-banner-text">
+        <h1><?php echo $bannerTitle; ?></h1>
+        <p><?php echo $bannerDescription; ?></p>
+    </div>
+</div>
--- root/index.php
+++ root/index.php
@@ -4,10 +4,12 @@
     require_once(DIR_BASE . 'objects/User.php');
     require_once(DIR_BASE . 'objects/Post.php');
 
+    $bannerTitle = "Welcome to Jae-yeon's Blog";
+    $bannerDescription = "This blog was created for educational purposes";
 ?>
 
 <?php include('inc/header.php'); ?>
-    
+    <?php include('components/banner.php'); ?>
     <?php include('pages/displayPosts.php'); ?>
 
 <?php include('inc/footer.php'); ?>
