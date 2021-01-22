1cb3f503d8560119c239311260fc9e5765ad90b4
leoyoon17 <l.yoon17@gmail.com>
2021-01-22 11:22:17 -0700

    added config file back

---
+++ root/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+    define('DIR_BASE', dirname( dirname(__FILE__)) . '/');
+    define('ROOT_URL', 'http://localhost/'); // Change for development on windows machines.
+    define('SITE_ROOT', dirname(__DIR__) . '/');
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'blogs');
+    define('DB_USER', 'jaeyeon');
+    define('DB_PASS', 'P4sswoid1!');
+?>
