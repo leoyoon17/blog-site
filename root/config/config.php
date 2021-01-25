b5247179e693cc4057fb42bd83fa9d3626c8f20b
Leo <l.yoon17@gmail.com>
2021-01-24 17:06:10 -0700

    fixed banner

--- root/config/config.php
+++ root/config/config.php
@@ -1,6 +1,7 @@
 <?php 
     define('DIR_BASE', dirname( dirname(__FILE__)) . '/');
-    define('ROOT_URL', 'http://localhost/'); // Change for development on windows machines.
+    define('ROOT_URL', 'http://localhost/phpsandbox/BlogSite/root/'); // For Windows development
+    // define('ROOT_URL', 'http://localhost/'); // Change for development on windows machines.
     define('SITE_ROOT', dirname(__DIR__) . '/');
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'blogs');
--- root/inc/footer.php
+++ root/inc/footer.php
@@ -1,9 +1,12 @@
 <!-- Note: the jquery script must come BEFORE the js script -->
-<script 
+        <script 
             src="https://code.jquery.com/jquery-3.5.1.js" 
             integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" 
             crossorigin="anonymous"></script>
 
         <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'> </script>
+
+        <!-- <script type="text/javascript" src="../js/test.js"></script> -->
+        <noscript>Your browser doesn't support or has disabled JavaScript</noscript>
     </body>
 </html>
